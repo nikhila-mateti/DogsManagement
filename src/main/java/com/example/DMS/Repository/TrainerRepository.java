8d4c2667b77bc2f8a87a6396c9a23c2dc12c0760
nikhila-mateti <s564202@nwmissouri.edu>
2024-04-16 22:41:51 -0500

    added dogsmanagement

---
+++ src/main/java/com/example/DMS/Repository/TrainerRepository.java
@@ -0,0 +1,10 @@
+package com.example.DMS.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Dog;
+import com.example.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer>{
+
+}
